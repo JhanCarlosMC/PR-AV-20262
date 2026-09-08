d00758f77a8b018ba41e6b64c0fcfd95d9c5ec53
JhanCarlosMC <jcmartinez@uniquindio.edu.co>
2026-09-08 08:42:55 -0500

    Domain: Explicacion creacion objetos de valor

---
+++ sga/src/main/java/co/edu/uniquindio/sga/domain/valueobject/DocumentoIdentidad.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.sga.domain.valueobject;
+
+import co.edu.uniquindio.sga.domain.exception.ReglaDominioException;
+
+public record DocumentoIdentidad(String numero) {
+
+    public DocumentoIdentidad{
+        if (numero == null || numero.isBlank() || numero.isEmpty()){
+            throw new ReglaDominioException("El documento de identidad es algo obligatorio, y no puede estar vacio.");
+        }
+        numero = numero.trim();
+    }
+}
